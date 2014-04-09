31263fb80ee1e9925265d1c9e1fd236a5d16a6c7
Zhiji Gu <gu_zhiji@163.com>
2014-04-10 00:06:30 +1000

    fixed a mis-fix to parse date and time; throw an exception when no records found & better exception handling

--- src/au/edu/unsw/cse/cs9322/assignment1/ImportDownloadServicesSkeleton.java
+++ src/au/edu/unsw/cse/cs9322/assignment1/ImportDownloadServicesSkeleton.java
@@ -32,16 +32,31 @@ import javax.servlet.http.HttpServletRequest;
 public class ImportDownloadServicesSkeleton implements ImportDownloadServicesSkeletonInterface {
 
     private static Calendar parseDateTime(String date, String time, String offset) throws ParseException {
+
+        //System.out.println("==parseDateTime()==");
+        //System.out.printf("date=%s, time=%s, offset=%s\n", date, time, offset);
+
         SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
         SimpleDateFormat tf = new SimpleDateFormat("HH:mm:ss.SSS", Locale.ENGLISH);
+        
+        // set timezone
         TimeZone tz = TimeZone.getTimeZone("GMT");
 //        TimeZone tz = TimeZone.getTimeZone("GMT" + offset);
         df.setTimeZone(tz);
         tf.setTimeZone(tz);
+        
+        // parse date and time
+        df.parse(date);
+        tf.parse(time);
+        
+        // add together
         Calendar d = df.getCalendar();
         Calendar t = tf.getCalendar();
         t.set(d.get(Calendar.YEAR), d.get(Calendar.MONTH), d.get(Calendar.DATE));
         t.add(Calendar.HOUR_OF_DAY, Integer.parseInt(offset));
+
+        //System.out.println(t.getTime().toString());
+
         return t;
     }
 
@@ -52,8 +67,8 @@ public class ImportDownloadServicesSkeleton implements ImportDownloadServicesSke
         File[] c = f.getParentFile() // webapps/axis2/WEB-INF/services
                 .getParentFile() // webapps/axis2/WEB-INF
                 .getParentFile() // webapps/axis2
-                .getParentFile(). // webapps
-                listFiles(new FilenameFilter() {
+                .getParentFile() // webapps
+                .listFiles(new FilenameFilter() {
                     public boolean accept(File dir, String name) {
                         return name.equalsIgnoreCase("root");
                     }
@@ -79,21 +94,24 @@ public class ImportDownloadServicesSkeleton implements ImportDownloadServicesSke
     }
 
     private static String getURL(String id) throws MalformedURLException {
+
         HttpServletRequest req = (HttpServletRequest) MessageContext.getCurrentMessageContext()
                 .getProperty(HTTPConstants.MC_HTTP_SERVLETREQUEST);
         URL url = new URL(req.getRequestURL().toString());
 
         StringBuilder result = new StringBuilder();
         result.append(url.getProtocol()).append(":");
-        if (url.getAuthority() != null && url.getAuthority().length() > 0) {
-            result.append("//").append(url.getAuthority());
-        }
+
+        String authority = url.getAuthority();
+        if (authority != null && authority.length() > 0)
+            result.append("//").append(authority);
+
         result.append('/').append(id).append(".csv");
         return result.toString();
+
     }
 
     /**
-     * Auto generated method signature
      *
      * @param importMarketData0
      * @return importMarketDataResponse1
@@ -103,54 +121,109 @@ public class ImportDownloadServicesSkeleton implements ImportDownloadServicesSke
             throws ImportDownloadFaultException {
 
         try {
-            ImportMarketDataDocument.ImportMarketData data = importMarketData0.getImportMarketData();
 
+            // read request
+            ImportMarketDataDocument.ImportMarketData data = importMarketData0.getImportMarketData();
             String reqSec = data.getSec();
             Calendar reqStartDate = data.getStartDate();// 2001-07-10T08:26:00Z
             Calendar reqEndDate = data.getEndDate();
 
+            // prepare CSV reader
+            URL url = new URL(data.getDataSourceURL());
+            CsvReader reader = new CsvReader(new InputStreamReader(url.openStream()));
+
+            // prepare CSV writer
             String root = findRoot();
             String id = prepareID(root);
-
             CsvWriter writer = new CsvWriter(root + id + ".csv");
-            URL url = new URL(data.getDataSourceURL()); // http://www.cse.unsw.edu.au/~hpaik/9322/assignments/common/files_csv_spec/FinDataSimple.csv
-            CsvReader reader = new CsvReader(new InputStreamReader(url.openStream()));
-            reader.readRecord();//skip headings
-            while (reader.readRecord()) {
-                String sec = reader.get(0);
-                if (!sec.equals(reqSec)) {
-                    continue;
-                }
-                String date = reader.get(1);
-                String time = reader.get(2);
-                String offset = reader.get(3);
-                Calendar t = parseDateTime(date, time, offset);
-                if (t.before(reqStartDate) || t.after(reqEndDate)) {
-                    continue;
-                }
-                writer.writeRecord(new String[]{
-                    sec,
-                    date,
-                    time,
-                    offset
-                }, true);
 
+            // import data
+            try {
+
+                try {
+
+                    boolean firstLine = true;
+                    boolean empty = true;
+                    while (reader.readRecord()) {
+
+                        // match security code
+                        String sec = reader.get(0);
+                        if (firstLine) {
+                            firstLine = false;
+                            if (sec.equals("#RIC"))
+                                continue; // skip headings
+                        }
+                        if (!sec.equals(reqSec))
+                            continue;
+
+                        // within the requested period
+                        String date = reader.get(1);
+                        String time = reader.get(2);
+                        String offset = reader.get(3);
+                        Calendar t = parseDateTime(date, time, offset);
+                        if (t.before(reqStartDate) || t.after(reqEndDate))
+                            continue;
+
+                        // write to file
+                        empty = false;
+                        writer.writeRecord(new String[]{
+                            sec,
+                            date,
+                            time,
+                            offset,
+                            reader.get(4), // type
+                            reader.get(5), // price
+                            reader.get(6), // volume
+                            reader.get(7), // bid price
+                            reader.get(8), // bid size
+                            reader.get(9), // ask price
+                            reader.get(10) // ask size
+                        }, true);
+
+                    }
+
+                    if (empty)
+                        throw new ImportDownloadFaultException("no records found");
+
+                } finally {
+
+                    try {
+                        writer.close();
+                    } catch (Exception ex) {
+                    }
+                    try {
+                        reader.close();
+                    } catch (Exception ex) {
+                    }
+
+                }
+
+            } catch (Exception ex) {
+
+                // delete corrupted file
+                File t = new File(root + id + ".csv");
+                if (t.exists())
+                    t.delete();
+
+                throw ex;
             }
-            reader.close();
-            writer.close();
 
+            // return id
             ImportMarketDataResponseDocument doc = ImportMarketDataResponseDocument.Factory.newInstance();
             ImportMarketDataResponseDocument.ImportMarketDataResponse resp = doc.addNewImportMarketDataResponse();
             resp.setEventSetId(id);
             return doc;
 
+        } catch (ImportDownloadFaultException ex) {
+            throw ex;
         } catch (Exception ex) {
+            // convert exception
             throw new ImportDownloadFaultException(ex);
         }
+
     }
 
     /**
-     * Auto generated method signature
      *
      * @param downloadFile2
      * @return downloadFileResponse3
@@ -160,20 +233,25 @@ public class ImportDownloadServicesSkeleton implements ImportDownloadServicesSke
             throws ImportDownloadFaultException {
 
         try {
+
+            // read request
             DownloadFileDocument.DownloadFile file = downloadFile2.getDownloadFile();
 
-            String root = findRoot();
             String id = file.getEventSetId();
+
+            // validate id
+            String root = findRoot();
             File t = new File(root + id + ".csv");
             if (!t.exists()) {
                 throw new FileNotFoundException("invalid event set id");
             }
 
+            // return url
             DownloadFileResponseDocument doc = DownloadFileResponseDocument.Factory.newInstance();
             DownloadFileResponseDocument.DownloadFileResponse resp = doc.addNewDownloadFileResponse();
             resp.setDataURL(getURL(id));
-
             return doc;
+
         } catch (Exception ex) {
             throw new ImportDownloadFaultException(ex);
         }
